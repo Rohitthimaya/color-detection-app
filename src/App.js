60afb1ca7e7baf6b6400b238350883181823dc5b
Rohitthimaya <thimayarohit@gmail.com>
2022-05-31 21:47:39 -0700

    Establised Event Handlers

--- src/App.js
+++ src/App.js
@@ -5,15 +5,32 @@ import ImageLinkForm from './Components/ImageFormLink/ImageFormLink';
 import Colors from './Components/Colors/Colors';
 import FaceRecognition from './Components/FaceRecognitionBox/FaceRecognitionBox';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Colors />
-      <ImageLinkForm />
-      <FaceRecognition />
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      input: ""
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonClicked = () => {
+    console.log("Clicked!")
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navigation />
+        <Colors />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonClicked={this.onButtonClicked} />
+        <FaceRecognition />
+      </div>
+    );
+  }
 }
 
 export default App;
