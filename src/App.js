5d8d4c3bc1f7cdc9290a3d5196270f94d828624c
Rohitthimaya <thimayarohit@gmail.com>
2022-05-31 22:37:38 -0700

    Working with failed import and response

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
-import React from 'react';
-import './App.css';
-import Navigation from './Components/Navigation/Navigation.js';
-import ImageLinkForm from './Components/ImageFormLink/ImageFormLink';
-import Colors from './Components/Colors/Colors';
-import FaceRecognition from './Components/FaceRecognitionBox/FaceRecognitionBox';
+import React from "react";
+import "./App.css";
+import Navigation from "./Components/Navigation/Navigation.js";
+import ImageLinkForm from "./Components/ImageFormLink/ImageFormLink";
+import Colors from "./Components/Colors/Colors";
+import FaceRecognition from "./Components/FaceRecognitionBox/FaceRecognitionBox";
+import Clarifai from 'clarifai';
+
+// API SETUP
+const app = new Clarifai.App({
+  apiKey: '7eb271450c424236b9156b56692b52ef'
+ });
 
 class App extends React.Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
-      input: ""
-    }
+      input: "",
+    };
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value)
-  }
+    console.log(event.target.value);
+  };
 
   onButtonClicked = () => {
-    console.log("Clicked!")
-  }
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        console.log('Res', response)
+      })
+      .catch(err => console.log(err));
+  };
 
-  render(){
+  render() {
     return (
       <div className="App">
         <Navigation />
         <Colors />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonClicked={this.onButtonClicked} />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonClicked={this.onButtonClicked}
+        />
         <FaceRecognition />
       </div>
     );
